ac717cade3bc5a7dc9d1f380bbb2b5098b9578e9
Geroge <spartan322@Live.com>
2014-03-12 11:44:24 -0400

    Update realism-mod.js

--- realism-mod.js
+++ realism-mod.js
@@ -12,7 +12,8 @@
 
         var error = function () {
         };
-        GDT.loadJs(['mods/gdt-modAPI/helpers/checks.js',
+        GDT.loadJs([
+        'mods/gdt-modAPI/helpers/checks.js',
         'mods/gdt-modAPI/api/persistence.js',
         'mods/gdt-modAPI/api/events.js',
         'mods/gdt-modAPI/api/platforms.js',

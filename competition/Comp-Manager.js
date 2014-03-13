dfb3566cabbfa674d7d7e08c75facb4881d70835
Geroge <spartan322@Live.com>
2014-03-13 12:54:44 -0400

    Update Comp-Manager.js

--- competition/Comp-Manager.js
+++ competition/Comp-Manager.js
@@ -69,3 +69,16 @@ CompManager.CompNameGenerator = function(){
         
         return Comp;
 };
+CompManager.dataStore = GDT.getDataStore("Realism-Mod");
+CompManager.save = function() {
+        if(Competitiors.entity.length == 0)
+                return;
+        
+        self.dataStore.data.cEntities = CompManager.comp;
+}();
+CompManager.startUp = function() {
+        GDT.on(GDT.eventKeys.saves.saving, CompManager.save);
+        GDT.on(GDT.eventKeys.saves.loading, function(){
+                CompManager.comp = self.dataStore.data.cEntities;
+        });
+}

58fda129e68139e4b9e97f62a8f77f5aee642c8d
Geroge <spartan322@Live.com>
2014-03-18 11:49:24 -0400

    Updated Current Events

--- event/events.js
+++ event/events.js
@@ -7,69 +7,78 @@ var Events = {};
                 for(var i = 0; i < Events.ID.length; i++)
                         Events.ID[i] = baseID + "Events-" + i;
         };
-        Events.GetEventID = function(id) {
-                return Events.ID[id];        
-        }
-        Events.rand[0] = {
+        Events.GetEventID = function() {
+                return Events.ID.pop();
+        };
+        Events.callGameRumor = function() {
                 Events.CreateEvent();
-                var thisID = Events.GetEventID(0);
-                id : thisID
-                isRandom : true
-                trigger : function (company) {
-                        return ReventKeys.keys.rumor.game;
+                var thisID = Events.GetEventID();
+                var e = {
+                        id : thisID,
+                        isRandom : false,
+                        maxTriggers : 1,
+                        trigger : function (company) {
+                                return true;
+                        },
+                        getNotification: function (company) {
+                                var game = company.currentGame;
+                                var msg = "News: It seems {0} has a new game coming to the market. They haven't comfirmed it but {0} seems to be finishing development on the game.".localize().format(Competitiors.entity[Competitiors.entity.length-1].name);
+                                var n = new Notification({
+                                        sourceId: thisID,//this is important, otherwise nothing will happen when the player selects an option.
+                                        header: "Rumor: New Game".localize(),//granted, this is a silly header.
+                                        text: msg
+                                });
+                                n.adjustHype(-5+-2*company.GetRandom());
+                                return n;
+                        }
                 }
-                getNotification: function (company) {
-                        var game = company.currentGame;
-                        var msg = "News: It seems {0} has a new game coming to the market. They haven't comfirmed it but {0} seems to be finishing development on the game.".localize().format(Competitiors.entity[Competitiors.entity.length-1].name);
-                        var n = new Notification({
-                                sourceId: thisID,//this is important, otherwise nothing will happen when the player selects an option.
-                                header: "Rumor: New Game".localize(),//granted, this is a silly header.
-                                text: msg
-                        });
-                        n.adjustHype(-5+-2*company.GetRandom());
-                        return n;
-                }        
-        }
-        GDT.addEvent(Events.rand[0]);
-        Events.rand[1] = {
+                GDT.addEvent(e);
+        };
+        Events.callEngineRumor = function() {
                 Events.CreateEvent();
-                var thisID = Events.GetEventID(1);
-                id : thisID
-                isRandom : true
-                trigger : function (company) {
-                        Competitiors.competition.CreateRandomCompetitior();
-                        return ReventKeys.keys.rumor.engine;
+                var thisID = Events.GetEventID();
+                var e = {
+                        id : thisID,
+                        isRandom : false,
+                        maxTriggers : 1,
+                        trigger : function (company) {
+                                return true;
+                        },
+                        getNotification: function (company) {
+                                var game = company.currentGame;
+                                var msg = "News: It seems {0} is creating a video game engine. It seems to be {1} but it hasn't been announced so we have no idea".localize().format(Competitiors.entity[Competitiors.entity.length-1].name,CheckTech());
+                                return new Notification({
+                                        sourceId: thisID,//this is important, otherwise nothing will happen when the player selects an option.
+                                        header: "Rumor: New Game Engine".localize(),//granted, this is a silly header.
+                                        text: msg
+                                });
+                        }
                 }
-                getNotification: function (company) {
-                        var game = company.currentGame;
-                        var msg = "News: It seems {0} is creating a video game engine. It seems to be {1} but it hasn't been announced so we have no idea".localize().format(Competitiors.entity[Competitiors.entity.length-1].name,CheckTech());
-                        return new Notification({
-                                sourceId: thisID,//this is important, otherwise nothing will happen when the player selects an option.
-                                header: "Rumor: New Game Engine".localize(),//granted, this is a silly header.
-                                text: msg
-                        });
-                }        
-        }
-        GDT.addEvent(Events.rand[1]);
-        Events.rand[2] = {
+                GDT.addEvent(e);
+        };
+        
+        Events.callConventionRumor = function() {
                 Events.CreateEvent();
-                var thisID = Events.GetEventID(2);
-                id : thisID
-                isRandom : true
-                trigger : function (company) {
-                        Competitiors.competition.CreateRandomCompetitior();
-                        return ReventKeys.keys.rumor.convention;
-                }
-                getNotification: function (company) {
-                        var game = company.currentGame;
-                        var msg = "News: It seems {0} is trying to start a game convention. Even the rumors of this convention are lowering {1}'s hype.".localize().format(Competitiors.entity[Competitiors.entity.length-1].name,game.title);
-                        var n = new Notification({
-                                sourceId: thisID,//this is important, otherwise nothing will happen when the player selects an option.
-                                header: "Rumor: New Game Convention".localize(),//granted, this is a silly header.
-                                text: msg
-                        });
-                        n.adjustHype(10 + 5*company.GetRandom());
-                }        
-        }
-        GDT.addEvent(Events.rand[2]);
+                var thisID = Events.GetEventID();
+                var e = {
+                        id : thisID,
+                        isRandom : false,
+                        maxTriggers : 1,
+                        trigger : function (company) {
+                                return true;
+                        },
+                        getNotification: function (company) {
+                                var game = company.currentGame;
+                                var msg = "News: It seems {0} is trying to start a game convention. Even the rumors of this convention are lowering {1}'s hype.".localize().format(Competitiors.entity[Competitiors.entity.length-1].name,game.title);
+                                var n = new Notification({
+                                        sourceId: thisID,//this is important, otherwise nothing will happen when the player selects an option.
+                                        header: "Rumor: New Game Convention".localize(),//granted, this is a silly header.
+                                        text: msg
+                                });
+                                n.adjustHype(10 + 5*company.GetRandom());
+                        }
+                } 
+                GDT.addEvent(e);
+        };
+        
 })();
